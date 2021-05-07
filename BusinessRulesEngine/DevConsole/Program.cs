27785b93f8bc62ab02f844370a0200b36455fcba
VoidEssy <mdknk231@gmail.com>
2021-05-07 16:53:34 +0200

    Initial flow demonstration and build up of rules

--- BusinessRulesEngine/DevConsole/Program.cs
+++ BusinessRulesEngine/DevConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DomainModels;
+using RuleEngine;
 using RuleEngine.Models;
 using System;
 using System.Collections.Generic;
@@ -10,20 +11,30 @@ namespace DevConsole
         static void Main(string[] args)
         {
             // Here will be a theoretical simplified execution of whole system simulated, goal is to show the engine in action and not create a whole system.
-            Rule physicalProduct = new Rule("IsService", "Equal", "false");
+            Rule physicalOrder = new Rule("IsService", "Equal", "false");
+            Rule serviceOrder = new Rule("IsService", "Equal", "true");
+            Rule bookOrder = new Rule("ProductType", "Equal", "Book");
+            Rule membershipOrder = new Rule("ProductType", "Equal", "Membership");
+            Rule upgradeOrder = new Rule("Operation", "Equal", "Upgrade");
+            Rule purchaseOrder = new Rule("Operation", "Equal", "Purchase");
+
+            foreach (Product order in GenerateProducts())
+            {
+                if (Engine.CompileRule<Product>(physicalOrder).Invoke(order))
+                {
+                    Console.WriteLine($"Packaging slip generated for {order.Name}");
+                }
+                else if (Engine.CompileRule<Product>(serviceOrder).Invoke(order))
+                {
+                    Console.WriteLine("This is a service");
+                }
+            }
+
+
 
         }
 
-        private static Dictionary<string, List<Product>> GenerateProducts()
-        {
-            Dictionary<string, List<Product>> products = new Dictionary<string, List<Product>>();
-            products.Add("Physical", GetListOfPhysicalProducts());
-            products.Add("Services", GetListOfServiceProducts());
-
-            return products;
-        }
-
-        private static List<Product> GetListOfPhysicalProducts()
+        private static List<Product> GenerateProducts()
         {
             return new List<Product>
             {
@@ -31,13 +42,6 @@ namespace DevConsole
                 new Product(false, "Book", "1984", "Purchase"),
                 new Product(false, "Video", "Terminator 10", "Purchase"),
                 new Product(false, "Book", "Animal Farm", "Purchase"),
-            };
-        }
-
-        private static List<Product> GetListOfServiceProducts()
-        {
-            return new List<Product>
-            {
                 new Product(true, "Membership", "User Usersen", "Activation"),
                 new Product(true, "Membership", "User Usersen", "Upgrade"),
                 new Product(true, "Video", "Learning to Ski", "Purchase"),
