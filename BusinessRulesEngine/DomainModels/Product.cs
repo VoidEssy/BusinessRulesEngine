3a4c0b6388e5967d346f4dcb059d354d69fec836
VoidEssy <mdknk231@gmail.com>
2021-05-07 15:48:58 +0200

    Expanded the Sample Domain / Business Model for illustratory case.

--- BusinessRulesEngine/DomainModels/Product.cs
+++ BusinessRulesEngine/DomainModels/Product.cs
@@ -6,11 +6,15 @@ namespace DomainModels
     {
         public bool IsService { get; set; }
         public string ProductType { get; set; }
+        public string  Name { get; set; }
+        public string Operation { get; set; }
 
-        public Product(bool isService, string productType)
+        public Product(bool isService, string productType, string name, string operation)
         {
             IsService = isService;
             ProductType = productType;
+            Name = name;
+            Operation = operation;
         }
 
         public Product()
